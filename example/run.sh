2add031534ca27ed1d2984972dff4dff337e2443
veto8 <veto@myridia.com>
2024-10-02 04:19:59 +0700

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,1 +1,27 @@
-docker run -i myridia/imapsync --tls1 --host1 foo.com --user1 foo@bar.com  --password1 mypass --tls2 --host2 foo2.com --user2 foo@bar.com  --password2  mypass
+#!/bin/bash
+source ./config.sh
+AUTH=$(echo -ne "$FROXLOR_API_KEY:$FROXLOR_API_SECRET" | base64 --wrap 0)
+
+while IFS="," read -r col1 col2 col3 col4 col5
+do
+  echo "user: $col1"
+  echo "mail: $col2"
+  echo "pass: $col3"
+  echo "email_part: $col4"
+  echo "domain: $col5"
+  echo 'domain: "'"$col5"'"  ' 
+  echo ""
+
+  
+docker run -i myridia/imapsync --tls1 \ 
+--host1 foo.com \
+--user1 foo@bar.com  \
+--password1 mypass \
+--tls2 --host2 foo2.com \
+--user2 foo@bar.com  \
+--password2  mypass
+
+
+
+done < mails.csv
+
